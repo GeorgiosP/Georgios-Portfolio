46292ae98eb9dd28f75ca9c8203fe4c5cc0e344b
GeorgiosP <g.psarakis3800@gmail.com>
2017-04-02 21:49:48 -0400

    Working on side nav logic so it doesn't show up at the top

--- scripts/sidenav.js
+++ scripts/sidenav.js
@@ -1,5 +1,33 @@
+
+const domReady = function(callback) {
+    document.readyState === "interactive" || document.readyState === "complete" ? callback() : document.addEventListener("DOMContentLoaded", callback);
+};
+
+/**
+ * Will grab the scroll location and if its not 
+ * 0 will show the side nav button
+ */
+document.domReady(function(){
+    window.addEventListener('scroll', function () {
+        let scrollPosition = document.body.scrollTop
+        const sideButton = document.getElementById("sideNav-button");
+
+        if (scrollPosition == 0) {
+            sideButton.style.visibility = "hidden";
+        } else {
+            sideButton.style.visibility = "visible";
+
+        }
+
+    })
+
+})
+
+
+
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.width = "30%";
 }
 
 function closeNav() {
