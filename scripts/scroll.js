f54a7d7cdf499d5b9da64e470d8ac18862e1f394
GeorgiosP <g.psarakis3800@gmail.com>
2017-07-25 04:46:37 -0400

    created logo and changed nav style

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,31 +3,33 @@ function getElementY(query) {
 }
 
 function doScrolling(element, duration) {
-	var startingY = window.pageYOffset
+  var startingY = window.pageYOffset
   var elementY = getElementY(element)
   // If element is close to page's bottom then window will scroll only to some position above the element.
   var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
-	var diff = targetY - startingY
+  var diff = targetY - startingY
   // Easing function: easeInOutCubic
   // From: https://gist.github.com/gre/1650294
-  var easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
+  var easing = function (t) {
+    return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1
+  }
   var start
 
   if (!diff) return
 
-	// Bootstrap our animation - it will get called right before next frame shall be rendered.
-	window.requestAnimationFrame(function step(timestamp) {
+  // Bootstrap our animation - it will get called right before next frame shall be rendered.
+  window.requestAnimationFrame(function step(timestamp) {
     if (!start) start = timestamp
     // Elapsed miliseconds since start of scrolling.
     var time = timestamp - start
-		// Get percent of completion in range [0, 1].
+    // Get percent of completion in range [0, 1].
     var percent = Math.min(time / duration, 1)
     // Apply the easing.
     percent = easing(percent)
 
     window.scrollTo(0, startingY + diff * percent)
 
-		// Proceed with animation as long as we wanted it to.
+    // Proceed with animation as long as we wanted it to.
     if (time < duration) {
       window.requestAnimationFrame(step)
     }
@@ -36,8 +38,8 @@ function doScrolling(element, duration) {
 
 document.getElementById('contactMe').addEventListener('click', doScrolling.bind(null, '#contact', 800))
 document.getElementById('homeLink').addEventListener('click', doScrolling.bind(null, '#home', 800))
+document.getElementById('homeLogo').addEventListener('click', doScrolling.bind(null, '#home', 800))
 document.getElementById('skillsLink').addEventListener('click', doScrolling.bind(null, '#skills', 800))
 document.getElementById('aboutLink').addEventListener('click', doScrolling.bind(null, '#about', 800))
 document.getElementById('projectsLink').addEventListener('click', doScrolling.bind(null, '#projects', 800))
 document.getElementById('contactLink').addEventListener('click', doScrolling.bind(null, '#contact', 800))
-
